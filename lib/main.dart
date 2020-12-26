7e0e2ce67bd6ff24a695c9d09fd9aedaaafc533e
Jefferson Moura Git <jeffermouramartins@gmail.com>
2020-12-26 15:42:20 -0300

    primeiros passos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Home()
+  );
 }
 
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
